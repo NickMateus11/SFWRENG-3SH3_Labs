49f696914536b2a8f98395b2397f591e79a9c5c4
NickMateus11 <mateus.nicholas@gmail.com>
2022-03-04 16:29:45 -0500

    Q2

--- Lab2/Q2.c
+++ Lab2/Q2.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
 #include <semaphore.h>
 #include <pthread.h>
+
+#define BUFF_SIZE   8
 
 pthread_t producer;
 pthread_t consumer;
@@ -9,48 +14,77 @@ sem_t empty;
 sem_t full;
 sem_t mutex;
 
-typedef struct {
-    int val;
-} item;
+int idx_insert = 0;
+int idx_consume = 0;
 
-void* insert_item(item* buffer) {
+void* insert_item(void (*buffer)) {
     while (1) {
         sem_wait(&empty);
         sem_wait(&mutex);
+
         // insert data
+        ((int*)(buffer))[idx_insert%BUFF_SIZE] = idx_insert;
+        printf("PRODUCER: Value Added: %d\r\n", idx_insert);
+        idx_insert++;
+
         sem_post(&mutex);
         sem_post(&full);
+        
+        sleep(rand()%3+1);
     }
 
 }
 
-void* remove_item(item* buffer) {
+void* remove_item(void (*buffer)) {
+    int idx = 0;
+    int val;
     while (1) {
         sem_wait(&full);
         sem_wait(&mutex);
+        
         // remove data
+        val = ((int*)(buffer))[idx_consume%BUFF_SIZE];
+        printf("CONSUMER: Value Consumed: %d\r\n", val);
+        idx_consume++;
+        
         sem_post(&mutex);
         sem_post(&empty);
+
+        sleep(rand()%3+1);
     }
 }
 
 void* main(int argc, char **argv) {
-    item buffer[10];
-    int in = 0;
-    int out = 0;
+    srand(time(NULL));
     
-    int sleep = atoi(argv[1]);
+    if (argc!=4){
+        printf("Not enough args\r\n");
+        exit(1);
+    }
+    int sleep_time = atoi(argv[1]);
     int numProducers = atoi(argv[2]);
     int numConsumers = atoi(argv[3]);
+
+    // Critical Sections
+    int arr[BUFF_SIZE]; // heap allocate?
     
-    pthread_create(&producer, NULL, insert_item, buffer);
-    pthread_create(&consumer, NULL, remove_item, buffer);
+    sem_init(&empty, 1, BUFF_SIZE); 
+    sem_init(&full , 1, 0);
+    sem_init(&mutex, 1, 1);
+
+    for (int i=0; i<numProducers; i++)
+        pthread_create(&producer, NULL, insert_item, arr);
     
-    sem_init(&empty, 0, 0); 
-    sem_init(&full, 0, 0);
-    sem_init(&mutex, 0, 0); // binary
-    
-    printf("Hello World");
+    for (int i=0; i<numConsumers; i++)    
+        pthread_create(&consumer, NULL, remove_item, arr);
+
+    printf("Sleeping for: %d\r\n", sleep_time);
+    sleep(sleep_time);    
+    printf("Sleeping DONE.. Exiting\r\n");
+
+    sem_destroy(&empty);
+    sem_destroy(&full);
+    sem_destroy(&mutex);
 
     return 0;
 }
